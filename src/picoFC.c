94308bcc3c4711d0b5ca24eca03a8dc927014c45
TamTamHero <10632523+TamtamHero@users.noreply.github.com>
2022-09-20 19:55:53 +0200

    Move mpu6050 readings to a FRTOS task

--- src/picoFC.c
+++ src/picoFC.c
@@ -4,100 +4,34 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+// FreeRTOS
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+
+// C
 #include <stdio.h>
 #include <string.h>
+
+// Pico SDK
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
+// Peripherals drivers
 #include "driver_mpu6050_interface.h"
 #include "mpu6050/driver_mpu6050.h"
-
-/* Example code to talk to a MPU6050 MEMS accelerometer and gyroscope
-
-   This is taking to simple approach of simply reading registers. It's perfectly
-   possible to link up an interrupt line and set things up to read from the
-   inbuilt FIFO to make it more useful.
-
-   NOTE: Ensure the device is capable of being driven at 3.3v NOT 5v. The Pico
-   GPIO (and therefor I2C) cannot be used at 5v.
-
-   You will need to use a level shifter on the I2C lines if you want to run the
-   board at 5v.
-
-   Connections on Raspberry Pi Pico board, other boards may vary.
-
-   GPIO PICO_DEFAULT_I2C_SDA_PIN (On Pico this is GP4 (pin 6)) -> SDA on MPU6050 board
-   GPIO PICO_DEFAULT_I2C_SCL_PIN (On Pico this is GP5 (pin 7)) -> SCL on MPU6050 board
-   3.3v (pin 36) -> VCC on MPU6050 board
-   GND (pin 38)  -> GND on MPU6050 board
-*/
 
 // By default these devices  are on bus address 0x68
 static int addr = 0x68;
 
-// static void mpu6050_reset() {
-//     // Two byte reset. First byte register, second byte data
-//     // There are a load more options to set up the device in different ways that could be added here
-//      uint8_t buffer[1] = {0};
+// This is the inter-task queue
+volatile QueueHandle_t queue = NULL;
 
-//     uint8_t buf[] = {0x6B, 0x00};
-//     i2c_write_blocking(i2c_default, addr, buf, 2, false);
+// Record references to the tasks
+TaskHandle_t mpu6050_task_handle = NULL;
 
-//     sleep_ms(1000);
-
-//     // read settings
-//     uint8_t val = 27;
-//     i2c_write_blocking(i2c_default, addr, &val, 1, true);
-//     i2c_read_blocking(i2c_default, addr, buffer, 1, false);
-//     printf("Gyro sensitivity: %d\n", buffer[0]);
-
-//     val = 28;
-//     i2c_write_blocking(i2c_default, addr, &val, 1, true);
-//     i2c_read_blocking(i2c_default, addr, buffer, 1, false);
-//     printf("Accel sensitivity: %d\n", buffer[0]);
-// }
-
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
-    // For this particular device, we send the device the register we want to read
-    // first, then subsequently read from the device. The register is auto incrementing
-    // so we don't need to keep sending the register we want, just the first.
-
-    uint8_t buffer[6] = {0};
-
-    // Start reading acceleration registers from register 0x3B for 6 bytes
-    uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
-    i2c_read_blocking(i2c_default, addr, buffer, 6, false);
-
-    for (int i = 0; i < 3; i++) {
-        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
-    }
-
-    // Now gyro data from reg 0x43 for 6 bytes
-    // The register is auto incrementing on each read
-    val = 0x43;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true);
-    i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
-
-    for (int i = 0; i < 3; i++) {
-        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
-    }
-
-    // Now temperature from reg 0x41 for 2 bytes
-    // The register is auto incrementing on each read
-    val = 0x41;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true);
-    i2c_read_blocking(i2c_default, addr, buffer, 2, false);  // False - finished with bus
-
-    *temp = buffer[0] << 8 | buffer[1];
-}
-
-int main() {
-    stdio_init_all();
-    sleep_ms(1000);
-    printf("Hello, MPU6050! Reading raw data from registers... %d %d\n", PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
-
+void task_mpu6050(void* unused_arg){
     mpu6050_basic_init(addr);
 
     float g[3];
@@ -122,5 +56,34 @@ int main() {
         sleep_ms(1000);
         count++;
     }
+}
+
+int main() {
+    stdio_init_all();
+    sleep_ms(1000);
+    printf("Hello, MPU6050! Reading raw data from registers... %d %d\n", PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
+
+    // Store handles referencing the task; get return value
+    // NOTE Arg 3 is the stack depth -- in words, not bytes
+    BaseType_t mpu6050_task_status = xTaskCreate(task_mpu6050,
+                                         "MPU6050_TASK",
+                                         512,
+                                         NULL,
+                                         1,
+                                         &mpu6050_task_handle);
+
+    // Set up the event queue
+    queue = xQueueCreate(4, sizeof(uint8_t));
+
+    // Start the FreeRTOS scheduler
+    if (mpu6050_task_status == pdPASS) {
+        vTaskStartScheduler();
+    }
+
+    // We should never get here, but just in case...
+    while(true) {
+        // NOP
+    };
+
     return 0;
 }
